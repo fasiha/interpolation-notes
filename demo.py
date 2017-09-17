243904fb3603975185337cc903431818b793b07c
Ahmed Fasih <wuzzyview@gmail.com>
2017-09-16 23:38:14 -0400

    Add docs and into a standalone module

--- demo.py
+++ demo.py
@@ -3,6 +3,29 @@ import scipy.signal as signal
 
 
 def remezBands(L, w0):
+    """Band edges and gains of an interpolation filter for use with Remez.
+
+    Parameters
+    ----------
+    L : int
+        Interpolation factor, e.g., 2, 3, etc.
+    w0 : float
+        Signal bandwidth in radians, i.e., 0 < `w0` < π.
+
+    Returns
+    -------
+    bands : ndarray
+        A rank-1 array (vector) of band edges in normalized frequency. All
+         entries are between 0 and 0.5.
+    gains : ndarray
+        Vector half-as-long as `bands` containing gains in these bands. These
+        two can be passed into, e.g., `scipy.signal.remez`.
+
+    References
+    ----------
+    .. [1] Phil Schniter, “ECE-700 Multirate Notes”, March 27, 2006, page 6.
+           http://www2.ece.ohio-state.edu/~schniter/ee700/handouts/multirate.pdf
+    """
     if L % 2 == 0:
         k = np.arange(0, L / 2.0)
         transitionBands = np.sort(np.hstack(
@@ -19,35 +42,52 @@ def remezBands(L, w0):
     return transitionBands, desired
 
 
-def viz(b):
-    import matplotlib.pyplot as plt
-    db20 = lambda x: 20 * np.log10(np.abs(x))
-    w, h = signal.freqz(b)
-    plt.plot(w / (2 * np.pi), db20(h))
-
-
 def remezDesign(L, w0, rippleDb=-30):
+    """Design an interpolation filter with Remez exchange.
+
+    Parameters
+    ----------
+    L : int
+        Interpolation factor, e.g., 2, 3, etc.
+    w0 : float
+        Bandwidth of the signal in radians, i.e., 0 < `w0` < π.
+    rippleDb : float
+        Tolerable ripple (in 10*log10 dB). This is used with Kaiser’s formula
+        to estimate the filter length. E.g., ripple (max in passband, min in
+        stopband) of 0.001 means `rippledB` of -30.
+
+    Returns
+    -------
+    out : ndarray
+        A rank-1 array (vector) containing the filter weights.
+    """
     bandsDesired = remezBands(L, w0)
     ntaps, _ = signal.kaiserord(rippleDb,
                                 (2 * np.pi - 2 * w0) / (L * 2 * np.pi))
     return signal.remez(ntaps, *bandsDesired)
 
 
-w0 = 0.9 * np.pi
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    db20 = lambda x: 20 * np.log10(np.abs(x))
 
-import matplotlib.pyplot as plt
+    def viz(b):
+        w, h = signal.freqz(b)
+        plt.plot(w / (2 * np.pi), db20(h))
 
-Ntaps = 96
-plt.figure()
-viz(signal.remez(Ntaps, *remezBands(5, w0)))
-viz(signal.remez(Ntaps, *remezBands(4, w0)))
-viz(signal.remez(Ntaps, *remezBands(3, w0)))
-viz(signal.remez(Ntaps, *remezBands(2, w0)))
-plt.grid()
+    w0 = 0.9 * np.pi
 
-plt.figure()
-viz(remezDesign(5, w0))
-viz(remezDesign(4, w0))
-viz(remezDesign(3, w0))
-viz(remezDesign(2, w0))
-plt.grid()
+    Ntaps = 96
+    plt.figure()
+    viz(signal.remez(Ntaps, *remezBands(5, w0)))
+    viz(signal.remez(Ntaps, *remezBands(4, w0)))
+    viz(signal.remez(Ntaps, *remezBands(3, w0)))
+    viz(signal.remez(Ntaps, *remezBands(2, w0)))
+    plt.grid()
+
+    plt.figure()
+    viz(remezDesign(5, w0))
+    viz(remezDesign(4, w0))
+    viz(remezDesign(3, w0))
+    viz(remezDesign(2, w0))
+    plt.grid()
